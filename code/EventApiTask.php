c43e75cf554c26c4a96326d3f749f5569c4e49cc
Paul Bennett <paul@moveforward.co.nz>
2013-10-08 10:43:58 +1300

    Switched EventApiTask from Controller to dedicated task

--- code/EventApiTask.php
+++ code/EventApiTask.php
@@ -1,11 +1,10 @@
 <?php 
 // EventApiTask is used to run periodic refresh from EF API 
-class EventApiTask extends Controller {
+class EventApiTask extends BuildTask {
 
-	private static $allowed_actions = array('index');
 
 	// controller action to be run by default
-	function index() {
+	function run() {
 
 		echo EventApi::test_connection();
 
