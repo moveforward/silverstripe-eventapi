88d4aa403ea9b34e984012240657e1cab11b0c4d
Paul Bennett <paul@moveforward.co.nz>
2013-10-08 13:11:51 +1300

    figuring out returned JSON structure

--- code/EventApi.php
+++ code/EventApi.php
@@ -96,7 +96,7 @@ class EventApi extends Extension {
 
 		$data = $this->api_connect($qs);
 
-		// print_r($data);
+		print_r($data);
 
 		// format as array
 		return Convert::json2array($data->events);
